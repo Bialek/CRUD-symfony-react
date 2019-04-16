ca089eb69765a6bcccf31f0f851a8d258b21800e
Białek Rafał <rafal.bialek@rfog.pl>
2019-04-16 14:30:35 +0200

    doctrine connect

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,61 +1,65 @@
 <?php
 
 namespace App\Controller;
-/**
- * @Route("/blog")
- */
+
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\Serializer;
+use App\Entity\BlogPost;
+
+/**
+ * @Route("/blog")
+ */
 
 class BlogController extends AbstractController {
-    private const POSTS = [
-        [
-            'id' => 1,
-            'slug' => 'hello-world',
-            'title' => 'Hello world!'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'another-post',
-            'title' => 'This is another post!'
-        ],
-        [
-            'id' => 3,
-            'slug' => 'last-example',
-            'title' => 'This is the last example'
-        ]
-    ];
-
+    
     /**
-     * @Route("/{page}", name="blog_list")
+     * @Route("/{page}", name="blog_list", defaults={"page": 5}, methods={"GET"}, requirements={"page"="\d+"})
      */
     public function list($page = 1, Request $request) {
         $limit = $request->get('limit', 10);
+        $repository = $this->getDoctrine()->getRepository(BlogPost::class);
+        $item = $repository->findAll();
 
         return $this->json([
             'page' => $page,
             'limit' => $limit,
-            'data' => array_map(function ($item) {
-                return $this -> generateUrl('blog_by_slug', ['slug' => $item['slug']]);
-            }, self::POSTS)
+            'data' => array_map(function (BlogPost $item) {
+                return $this -> generateUrl('blog_by_slug', ['slug' => $item->getSlug()]);
+            }, $item)
         ]);
     }
     /**
-     * @Route("/id", name="blog_by_id", requirements={"id"="\d+"})
+     * @Route("/post/{id}", name="blog_by_id", requirements={"id"="\d+"})
      */
     public function post($id) {
         return $this->json(
-            self::POSTS[array_serch($id, array_column(self::POSTS, 'id'))]
+            $this->getDoctrine()->getRepository(BlogPost::class)->find($id)
         );
     }
     /**
-     * @Route("/{slug}", name="blog_by_slug")
+     * @Route("/post/{slug}", name="blog_by_slug")
      */
     public function postBySlug($slug) {
         return $this->json(
-            self::POSTS[array_search($slug, array_column(self::POSTS,'slug'))]
+            $this->getDoctrine()->getRepository(BlogPost::class)->findOneBy(['slug' => $slug])
         );
+    }
+    /**
+     * @Route("/add", name="blog_add", methods={"POST"})
+     */
+    public function add(Request $request) {
+        /** @var Serializer $serializer */
+        $serializer = $this->get('serializer');
+
+        $blogPost = $serializer->deserialize($request->getContent(), BlogPost::class, 'json');
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($blogPost);
+        $em->flush();
+
+        return $this->json($blogPost);
     }
 }
--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -6,9 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/")
- */
 class DefaultController extends AbstractController {
     /**
      * @Route("/", name="default_index")
@@ -19,4 +16,5 @@ class DefaultController extends AbstractController {
             'time' => time()
         ]);
     }
+    
 }
